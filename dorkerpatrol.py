1486556dadee21a13228e5dcc4bb1ad2a6df0b04
Tim Schughart <t.schughart@prosec-networks.com>
2015-10-31 14:08:43 +0100

    Initial

---
+++ dorkerpatrol.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+__author__ = 'Tim Schughart'
+
+import configparser
+import logging
+import lib.comparer as comparer
+import lib.dorker as dorker
+import lib.exploiter as exploiter
+import urllib3
+import sys
+
+def request(url):
+    conn = urllib3.
+    return conn
+
+
+def crawler(dork):
+    pass
+
+print(request("www.google.de").request())
